3244a538134e2191bb81995a917be754cf45d1ff
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2019-03-22 00:27:21 +0100

    working on the question preview layout

--- src/components/QuestionPreview.js
+++ src/components/QuestionPreview.js
@@ -1,9 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import {Row, Col, Image,Card, Button, Container } from 'react-bootstrap'
+var placeholder = require('../images/paceholder.svg')
 
-class QuestionPreview extends Component{
-  render(){
-    return(
-      <li>QuestionPreview</li>
+class QuestionPreview extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          Name Sirname askes:
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col>
+            <Image src={placeholder} roundedCircle  />
+          </Col>
+          <Col  >
+            the content of the big column
+          </Col>
+        
+        </Row>
+      </Container>
     )
   }
 }
--- src/components/QuestionPreviewContainer.js
+++ src/components/QuestionPreviewContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import QuestionPreview from './QuestionPreview'
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap'
+
 
 class QuestionPreviewContainer extends Component {
   render() {
