abeddb0536ef250cdd05a570597a50a271b10b8b
bogdan <bogdan.oloeriu@around.media>
2019-03-27 09:34:40 +0100

    fixed listing users

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,42 @@
 import React, { Component, Fragment } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import { Container } from "react-bootstrap";
 import QuestionNavigation from "./QuestionNavigation";
 import QuestionPreviewContainer from "./QuestionPreviewContainer";
 import QuestionNavbar from "./QuestionNavbar";
-import LoadingBar from 'react-redux-loading'
-import QuestionPage from './QuestionPage'
-import Lader from './Lader'
-import NewQuestion from './NewQuestion'
-import { fakeAuth } from '../utils/helpers'
-
+import LoadingBar from "react-redux-loading";
+import QuestionPage from "./QuestionPage";
+import Lader from "./Lader";
+import NewQuestion from "./NewQuestion";
+import { fakeAuth } from "../utils/helpers";
 
 class Login extends React.Component {
   state = {
     redirectToRefferrer: false
-  }
+  };
   login = () => {
     fakeAuth.authenticate(() => {
       this.setState(() => ({
         redirectToRefferrer: true
-      }))
-    })
-  }
+      }));
+    });
+  };
 
   render() {
     const { users } = this.props;
+    var listUsers = Object.keys(users).map(key => (
+      <li key={key}>
+        <span>{users[key].name}</span>
+      </li>
+    ));
 
-
-    
-
-    var listUsers = Object.keys(users).map((key, obj) =>(
-      <li key={key}>Name: {obj}{key}{users.key}</li>
-    ))
-
-    const { redirectToRefferrer } = this.state
-    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToRefferrer } = this.state;
+    const { from } = this.props.location.state || { from: { pathname: "/" } };
 
     if (redirectToRefferrer === true) {
-      return (
-        <Redirect to={from} />
-      )
+      return <Redirect to={from} />;
     }
 
     return (
@@ -53,26 +44,29 @@ class Login extends React.Component {
         <p>You must log in to view this page at {from.pathname}</p>
         <button onClick={this.login}>Log in </button>
 
-        <ul>
-          {listUsers}
-        </ul>
+        <ul>{listUsers}</ul>
       </div>
-    )
-
+    );
   }
 }
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    fakeAuth.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to={{
-        pathname: '/login',
-        state: { from: props.location }
-      }} />
-  )} />
-)
-
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
 
 class App extends Component {
   componentDidMount() {
@@ -87,19 +81,35 @@ class App extends Component {
 
           <div>
             <LoadingBar />
-            {this.props.loading === true
-              ? null
-              : <div>
-                <PrivateRoute path='/' exact component={QuestionPreviewContainer} />
-                <Route path='/login' component={(props) => <Login {...props} users={this.props.users} />} />
-                <PrivateRoute path='/question/:id' exact component={QuestionPage} />
-                <PrivateRoute path='/lader' exact component={Lader} />
-                <PrivateRoute path='/newQuestion' exact component={NewQuestion} />
-              </div>}
+            {this.props.loading === true ? null : (
+              <div>
+                <PrivateRoute
+                  path="/"
+                  exact
+                  component={QuestionPreviewContainer}
+                />
+                <Route
+                  path="/login"
+                  component={props => (
+                    <Login {...props} users={this.props.users} />
+                  )}
+                />
+                <PrivateRoute
+                  path="/question/:id"
+                  exact
+                  component={QuestionPage}
+                />
+                <PrivateRoute path="/lader" exact component={Lader} />
+                <PrivateRoute
+                  path="/newQuestion"
+                  exact
+                  component={NewQuestion}
+                />
+              </div>
+            )}
           </div>
         </Container>
       </Router>
-
     );
   }
 }
