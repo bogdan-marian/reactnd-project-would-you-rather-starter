817c11fd90f3fceff8d512c186ce8ad514d77f68
bogdan <bogdan.oloeriu@around.media>
2019-03-29 15:34:44 +0100

    updated form

--- src/components/AnswerQuestion.js
+++ src/components/AnswerQuestion.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { formatQuestion } from '../utils/helpers'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatQuestion } from "../utils/helpers";
+import { Link } from "react-router-dom";
 import {
-  Row, Col, Image, Container, Button, Card, ButtonGroup, ToggleButton,
-  ToggleButtonGroup, ButtonToolbar, Form
+  Row,
+  Col,
+  Image,
+  Container,
+  Button,
+  Card,
+  ButtonGroup,
+  ToggleButton,
+  ToggleButtonGroup,
+  ButtonToolbar,
+  Form
 } from "react-bootstrap";
 import styles from "./Question.module.css";
 
@@ -15,7 +24,7 @@ class ToggleButtonGroupControlled extends React.Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
-      value: [1, 3],
+      value: [1, 3]
     };
   }
 
@@ -26,95 +35,68 @@ class ToggleButtonGroupControlled extends React.Component {
   render() {
     return (
       <div>
-        <Row><ToggleButtonGroup
-          type="checkbox"
-          value={this.state.value}
-          onChange={this.handleChange}
-        >
-          <ToggleButton value={1}>Option 1</ToggleButton>
-          <ToggleButton value={2}>Option 2</ToggleButton>
-          <ToggleButton value={3}>Option 3</ToggleButton>
-        </ToggleButtonGroup>
+        <Row>
+          <ToggleButtonGroup
+            type="checkbox"
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
+            <ToggleButton value={1}>Option 1</ToggleButton>
+            <ToggleButton value={2}>Option 2</ToggleButton>
+            <ToggleButton value={3}>Option 3</ToggleButton>
+          </ToggleButtonGroup>
         </Row>
-
       </div>
-
     );
   }
 }
 
 class AnswerQuestion extends Component {
-
   render() {
-    const { question } = this.props
-    const { name, id, avatar, optionOne, optionTwo } = question
+    const { question } = this.props;
+    const { name, id, avatar, optionOne, optionTwo } = question;
     return (
       <Container className={styles.questionWidth}>
-        <Row />
-        <Row className={styles.nameStile}>{name} askes:</Row>
-        <Row className="justify-content-md-center">
-          <Col md="auto">
-            <Image
-              src={avatar}
-              roundedCircle className={styles.avatarWidth} />
-          </Col>
-          <Col >
-            <div className={styles.rotherStile}>Whould you rather</div>
-            <Row noGutters >{optionOne.text}</Row>
-            <Row noGutters className="justify-content-md-center">
-              <Col xs lg="1">or</Col>
-            </Row>
-            <Row noGutters >{optionTwo.text}</Row>
-            <Row className="justify-content-md-center">
-              <Col>
-              </Col>
-              <Col>
-                <Link to={"/question/" + id}><Button variant="outline-secondary">  View Pool</Button></Link>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row />
-
         <Card>
           <Card.Header>{name} askes:</Card.Header>
           <Card.Body>
-
-
             <Row>
-              <Col xs={4} >
+              <Col xs={4}>
                 <Image
                   src={avatar}
-                  roundedCircle className={styles.avatarWidth} />
+                  roundedCircle
+                  className={styles.avatarWidth}
+                />
               </Col>
-              <Col xs={8}><Card.Title >Would You Rather ... </Card.Title>
+              <Col xs={8}>
+                <Card.Title>Would You Rather ... </Card.Title>
 
-                <Row noGutters >{optionOne.text}</Row>
+                <Row noGutters>{optionOne.text}</Row>
 
-
-                <Row><Button variant="primary">Go somewhere</Button></Row>
+                <Form.Check
+                  type="radio"
+                  label={question.optionOne.text}
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label={question.optionTwo.text}
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
+                />
+                <Row>
+                  <Button variant="primary">Submit</Button>
+                </Row>
               </Col>
             </Row>
-
-            <Form.Check
-              type="radio"
-              label="first radio"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios1"
-            />
-            <Form.Check
-              type="radio"
-              label="second radio"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios2"
-            />
-
           </Card.Body>
         </Card>
-
       </Container>
-    )
+    );
   }
 }
 
-export default AnswerQuestion
+map;
+
+export default AnswerQuestion;
--- src/components/QuestionPreview.js
+++ src/components/QuestionPreview.js
@@ -1,58 +1,57 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import { Row, Col, Image, Container, Button } from "react-bootstrap";
 import styles from "./Question.module.css";
-import { formatQuestion } from '../utils/helpers'
-import { Link } from 'react-router-dom'
+import { formatQuestion } from "../utils/helpers";
+import { Link } from "react-router-dom";
 
 class QuestionPreview extends Component {
   render() {
-    const { question } = this.props
+    const { question } = this.props;
     if (question === null) {
-      return <p> This question doesn't exist</p>
+      return <p> This question doesn't exist</p>;
     }
-    const { name, id, avatar, optionOne, optionTwo } = question
+    const { name, id, avatar, optionOne, optionTwo } = question;
     return (
-
       <Container className={styles.questionWidth}>
-
         <Row />
         <Row className={styles.nameStile}>{name} askes:</Row>
         <Row className="justify-content-md-center">
           <Col md="auto">
-            <Image
-              src={avatar}
-              roundedCircle className={styles.avatarWidth} />
+            <Image src={avatar} roundedCircle className={styles.avatarWidth} />
           </Col>
-          <Col >
+          <Col>
             <div className={styles.rotherStile}>Whould you rather</div>
-            <Row noGutters >{optionOne.text}</Row>
+            <Row noGutters>{optionOne.text}</Row>
             <Row noGutters className="justify-content-md-center">
-              <Col xs lg="1">or</Col>
+              <Col xs lg="1">
+                or
+              </Col>
             </Row>
-            <Row noGutters >{optionTwo.text}</Row>
+            <Row noGutters>{optionTwo.text}</Row>
             <Row className="justify-content-md-center">
-              <Col></Col>
+              <Col />
               <Col>
-                <Link to={"/question/"+id}><Button variant="outline-secondary">  View Pool</Button></Link>
+                <Link to={"/question/" + id}>
+                  <Button variant="outline-secondary"> View Poll</Button>
+                </Link>
               </Col>
             </Row>
           </Col>
         </Row>
         <Row />
       </Container>
-
-    )
+    );
   }
 }
 
 function mapStateToProps({ authedUser, users, questions }, { id, section }) {
-  const question = questions[id]
+  const question = questions[id];
   return {
     authedUser,
     question: formatQuestion(question, users[question.author], authedUser),
     section: section
-  }
+  };
 }
 
-export default connect(mapStateToProps)(QuestionPreview)
+export default connect(mapStateToProps)(QuestionPreview);
